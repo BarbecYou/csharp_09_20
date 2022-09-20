19985db74927e7f23299116d3e1e803de5bc49a5
Füredi Dániel <daniel.furedi03@gmail.com>
2022-09-20 17:04:52 +0200

    Add project files.

---
+++ 09_20/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09_20
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Feladat15();
+            Console.Read();
+        }
+
+        private static void Feladat15()
+        {
+            double[] tomb = new double[25];
+            Random rnd = new Random();
+            for (int i = 0; i < tomb.Length; i++)
+            {
+                tomb[i] = rnd.NextDouble() * 100 - 50;
+            }
+
+            for (int i = 1; i < tomb.Length; i+=2)
+            {
+                Console.WriteLine(tomb[i] * tomb[i]);
+            }
+        }
+    }
+}
