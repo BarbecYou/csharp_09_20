f42690d2b6504b6aaee76162693243b3c80c434e
Füredi Dániel <daniel.furedi03@gmail.com>
2022-09-20 18:14:50 +0200

    finished Pont class

---
+++ 09_20/Pont.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09_20
+{
+    public class Pont
+    {
+        private int x;
+        private int y;
+        private static Random rnd = new Random();
+
+        public Pont(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public Pont()
+        {
+            x = 0;
+            y = 0;
+        }
+
+        
+
+        public Pont(int n)
+        {
+            this.x = rnd.Next(-n, n + 1);
+            this.y = rnd.Next(-n, n + 1);
+        }
+
+        public int X { get => x; set => x = value; }
+        public int Y { get => y; set => y = value; }
+
+        public double TavolsagAzOrigotol
+        {
+            get
+            {
+                return Math.Sqrt(this.x * this.x + this.y * this.y);
+            }
+        }
+
+        public static Pont OrigotolLegtavolabb(Pont[] tomb)
+        {
+            double tempTavolsag = 0;
+            Pont tempPont = null;
+            for (int i = 0; i < tomb.Length; i++)
+            {
+                if (tomb[i].TavolsagAzOrigotol > tempTavolsag)
+                {
+                    tempTavolsag = tomb[i].TavolsagAzOrigotol;
+                    tempPont = tomb[i];
+                }
+            }
+            return tempPont;
+        }
+
+        public override string ToString()
+        {
+            return $"Koordináták: {{{x}; {y}}}";
+        }
+    }
+}
--- 09_20/Program.cs
+++ 09_20/Program.cs
@@ -10,9 +10,38 @@ namespace _09_20
     {
         static void Main(string[] args)
         {
-            Feladat15();
+            //Feladat15();
+
+            PontFeladat();
+
             Console.Read();
         }
+
+        private static void PontFeladat()
+        {
+            Pont pont1 = new Pont();
+            Pont pont2 = new Pont(3, 4);
+            Pont pont3 = new Pont(100);
+
+            Console.WriteLine(pont1);
+            Console.WriteLine(pont2);
+            Console.WriteLine(pont3);
+
+            Pont[] tomb = new Pont[100];
+            for (int i = 0; i < tomb.Length; i++)
+            {
+                tomb[i] = new Pont(10);
+            }
+            foreach (Pont pont in tomb)
+            {
+                Console.WriteLine(pont);
+            }
+
+            Console.WriteLine($"{Pont.OrigotolLegtavolabb(tomb)} tavolsaga: {Pont.OrigotolLegtavolabb(tomb).TavolsagAzOrigotol:0.000}");
+
+            Console.WriteLine(tomb.First(x => x.TavolsagAzOrigotol == tomb.Max(y => y.TavolsagAzOrigotol)));
+            
+        }   
 
         private static void Feladat15()
         {
